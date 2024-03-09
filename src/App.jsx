de0d1ddbcca30aae89de27fe10c3d7c32cdcd383
calumtreloar <calumtreloar@gmail.com>
2024-03-09 23:14:23 +1030

    tours-project

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+
+import Loading from "./components/Loading";
+
+import Tours from "./components/Tours";
+
+const url = "https://course-api.com/react-tours-project";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = id => {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setTours(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button type="button" className="btn" style={{ marginTop: "2rem" }} onClick={fetchTours}>
+            reset tours
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+};
+export default App;
